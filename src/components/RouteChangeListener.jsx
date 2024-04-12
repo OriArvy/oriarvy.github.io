a3ed7bb65bc5bec4977057e340253fd7e18d9c8b
Rokas <rokas.vaitkevicius1@gmail.com>
2024-04-12 13:32:07 +0300

    Removed 404 redirect changes in RouteChangeListener and added a script in index.hmtl

--- src/components/RouteChangeListener.jsx
+++ src/components/RouteChangeListener.jsx
@@ -1,18 +1,8 @@
-import { useLocation, useNavigate } from "react-router-dom";
+import { useLocation,  } from "react-router-dom";
 import { useEffect } from "react";
 
 function RouteChangeListener() {
   const location = useLocation()
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const urlParams = new URLSearchParams(window.location.search);
-    const path = urlParams.get('path');
-    if (path) {
-      navigate(path);
-    }
-  }, [navigate]);
-
 
   useEffect(() => {
     localStorage.removeItem('playlists')
